0128f4c064bf0a58d19cc05e6da627fe65cad55d
chris <i_cristian32@yahoo.com>
2019-06-14 19:21:15 +0300

    Second instant

--- app/src/main/java/com/example/app01/MainActivity.java
+++ app/src/main/java/com/example/app01/MainActivity.java
@@ -7,19 +7,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button clockin1;
+    Button clockout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button clockin1 = (Button) findViewById(R.id.clockin);
-        Button clockout1 = (Button) findViewById(R.id.clockout);
+        clockin1 = (Button) findViewById(R.id.clockin);
+        clockout1 = (Button) findViewById(R.id.clockout);
 
         clockin1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 System.out.println("lalala");
+                Singleton01.getInstance().setVisibleSecondButton(true);
+                Singleton01.getInstance().setVisibleFirstButton(false);
+
+                visibleButtons();
             }
         });
 
@@ -27,9 +33,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 System.out.println("blabla");
+                Singleton01.getInstance().setVisibleFirstButton(true);
+                Singleton01.getInstance().setVisibleSecondButton(false);
+                visibleButtons();
             }
         });
 
+    }
+
+    private void visibleButtons() {
+        if(Singleton01.getInstance().isVisibleSecondButton()) {
+            clockin1.setEnabled(false);
+            clockout1.setEnabled(true);
+        }
+        if(Singleton01.getInstance().isVisibleFirstButton() ) {
+            clockin1.setEnabled(true);
+            clockout1.setEnabled(false);
+        }
     }
 }
 
--- app/src/main/java/com/example/app01/Singleton01.java
+++ app/src/main/java/com/example/app01/Singleton01.java
@@ -1,6 +1,10 @@
 package com.example.app01;
 
 class Singleton01 {
+
+    boolean VisibleSecondButton;
+    boolean VisibleFirstButton;
+
     private static final Singleton01 ourInstance = new Singleton01();
 
     static Singleton01 getInstance() {
@@ -9,4 +13,22 @@ class Singleton01 {
 
     private Singleton01() {
     }
+
+    public boolean isVisibleSecondButton() {
+        return VisibleSecondButton;
+    }
+
+    public void setVisibleSecondButton(boolean visibleSecondButton) {
+        VisibleSecondButton = visibleSecondButton;
+    }
+
+    public boolean isVisibleFirstButton() {
+        return VisibleFirstButton;
+    }
+
+    public void setVisibleFirstButton(boolean visibleFirstButton) {
+        VisibleFirstButton = visibleFirstButton;
+    }
+
+
 }
